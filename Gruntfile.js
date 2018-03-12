8952bbe599f160c56fccce65161c34ed0a89e93d
Chris Bishop <domingobishop@gmail.com>
2018-03-12 16:24:20 +0000

    first commit

---
+++ Gruntfile.js
@@ -0,0 +1,41 @@
+module.exports = function (grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        sass: {
+            options: {
+                sourcemap: 'none'
+            },
+            dist: {
+                files: {
+                    'tna-base.css': 'sass/base-sass.scss'
+                }
+            }
+        },
+        cssmin: {
+            options: {
+                sourceMap: true
+            },
+            target: {
+                files: {
+                    'tna-base.css.min': ['tna-base.css']
+                }
+            }
+        },
+        watch: {
+            css: {
+                files: 'sass/*.scss',
+                tasks: ['sass', 'cssmin']
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+
+    // Default task(s).
+    grunt.registerTask('default', ['sass', 'cssmin', 'watch']);
+
+};
